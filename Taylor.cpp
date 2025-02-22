4a44e7ea3a5f191bdedd2fbb8c154e67d3828863
electro-geek <mritunjaypandey0789@gmail.com>
2025-02-23 00:11:48 +0530

    First Commit

---
+++ Taylor.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+class NewMath{
+    public:
+        int factorial(int n){
+            if(n == 0){
+                return 1;
+            }
+            return n * factorial(n-1);
+        }
+}
